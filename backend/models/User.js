0d75e69e93f0ad4e23d0b5b41a3a6b6750338ed6
CGuillotG <c.guillot@outlook.com>
2019-03-07 18:16:51 -0600

    Created auth route, passport helper, and User model

---
+++ backend/models/User.js
@@ -0,0 +1,53 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+let passportLocalMongoose = require('passport-local-mongoose')
+
+
+let userSchema = new Schema({
+  username: {
+    type:String,
+    unique: true,
+    required: true
+  },
+  firstname: {
+    type:String,
+    required: true
+  },
+  lastname: {
+    type:String,
+    required: true
+  },
+  email: {
+    type:String,
+    unique: true,
+    required: true
+  },
+  phone: {
+    type:String,
+    unique: true,
+    required: true
+  },
+  profilePic:{
+    type:String,
+    default: "../../resources/icons/avatar.png"
+  },
+  ratingLessor:Number,
+  ratingLessee:Number,
+  address:{},
+  payment:{},
+  deposit:{},
+  isAdmin: {
+    type:Boolean,
+    default:false
+  },
+  // prodsLessor: [],
+  // prodsLessee: []
+},{
+  timestamps:true,
+  versionKey:false
+})
+
+
+userSchema.plugin(passportLocalMongoose,{usernameField:"email"})
+
+module.exports = mongoose.model('User', userSchema)
---
+++ backend/routes/auth.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const passport = require('passport')
+const uploadCloud = require('../helpers/cloudinary')
+
+//Middle wares
+function isAuth(req, res, next) {
+  if (req.isAuthenticated()) next()
+  else res.status(401).json({ message: "You haven't logged in yet." })
+}
+
+//edit
+router.post('/edit', isAuth, uploadCloud.single('profilePic'), (req, res, next) => {
+  if (req.file) req.body.profilePic = req.file.secure_url
+  User.findByIdAndUpdate(req.user._id, { ...req.body })
+    .then(user => res.status(200).json({message: 'Changes updated sucessfully', changes:{...req.body}}))
+    .catch(e => console.log(e))
+})
+
+//Sign up
+router.post('/signup', (req, res, next) => {
+  User.register(req.body, req.body.password)
+    .then(user => res.status(201).json(user))
+    .catch(e => next(e))
+})
+
+//Login
+router.post('/login', passport.authenticate('local'), (req, res, next) => {
+  console.log(req.user)
+  res.status(200).json(req.user)
+})
+
+//Privates
+router.get('/admin', isAuth, (req, res, next) => {
+  res.status(200).json({ message: 'Access granted: Admin', user: req.user })
+})
+
+//Logout
+router.get('/logout', isAuth, (req, res, next) => {
+  req.logout()
+  req.session.destroy(err => {
+    if (!err) res.status(200).clearCookie('connect.sid', { path: '/' }).json({ message: 'Logout successful' })
+  })
+})
+
+module.exports = router
