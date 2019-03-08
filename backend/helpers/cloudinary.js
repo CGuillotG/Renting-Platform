f1a3a5e491e0fa546df97025899466e49d0fbbb9
CGuillotG <c.guillot@outlook.com>
2019-03-07 18:17:00 -0600

    created Cloudinary helper

---
+++ backend/helpers/cloudinary.js
@@ -0,0 +1,23 @@
+const cloudinary = require("cloudinary");
+const multer = require("multer");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+});
+
+let storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "reactingBeers",
+  // folder: process.env.CLOUDINARY_FOLDER,
+  allowedFormats: ["jpg", "png", "gif", "jpeg"],
+  filename: (req, file, cb) => {
+    cb(null, file.filename);
+  }
+});
+
+const uploadCloud = multer({ storage: storage });
+
+module.exports = uploadCloud;
