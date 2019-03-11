946c00b3b9d32e69ef6f8a234371e203d7d90dd4
CGuillotG <c.guillot@outlook.com>
2019-03-11 17:45:05 -0600

    Finished users.js service

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -5,16 +5,40 @@ let url = `${process.env.BACKURL}/auth`
 
 
 //login
-export const logIn = (auth) => {
-  axios.post(url, auth, { withCredentials: true })
+export const logIn = auth => {
+  return axios.post(`${url}/login`, auth, { withCredentials: true })
   .then(res => res.data)
 }
 
-// // get all
-// export const getBooks = () => axios.get(url).then(res => res.data)
+//logout
+export const logOut = () => {
+  return axios.post(`${url}/logout`, { withCredentials: true })
+  .then(res => res.data)
+}
 
-// //subir un libro
-// export function postBook(book) {
-//     return axios.post(url, book)
-//         .then(res => res.data)
-// }
+//logged
+export const logged = () => {
+  return axios.post(`${url}/logged`, { withCredentials: true })
+  .then(res => res.data)
+}
+
+//signup
+export const signup = newUser => {
+  return axios.post(`${url}/signup`, newUser)
+  .then(res => res.data)
+}
+
+//edit
+export const edit = updateUser => {
+  axios.post(url, {...updateUser}, { withCredentials: true })
+  .then(res => res.data)
+}
+export const imageUpload = file => {  
+  let imageurl = `${url}/edit`
+  let formData = new FormData()
+  formData.append("profilePic", file)
+  const serviceUpload = axios.create({ imageurl, withCredentials: true })
+  serviceUpload.post(url, formData, {headers: { enctype: "multipart/form-data" }})
+  .then(res => res.data)
+  .catch(e => e)
+}
