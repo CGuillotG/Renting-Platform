a4a8930a6c6055b4fb735c39a318d87af35489ee
CGuillotG <c.guillot@outlook.com>
2019-03-12 12:37:33 -0600

    Added searchbar functionality

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 let requestsURL = 'http://localhost:3000/requests/'
 let productsURL = 'http://localhost:3000/products/'
 
 export default class Beers extends React.Component {
   state = {
     requests: [],
-    products: []
+    products: [],
+    searchterm: null
   }
 
   componentDidMount = () => {
@@ -15,58 +17,69 @@ export default class Beers extends React.Component {
       this.setState({ requests })
     })
     axios.get(productsURL).then(res => {
-      let products = res.data.sort(function (a, b) {
-        if (a.createdAt < b.createdAt) {
-          return 1;
-        }
-        if (a.name > b.name) {
-          return -1;
-        }
-        return 0;
-      });
+      let products = res.data
       if(products.length > 5) products.length = 5
       this.setState({ products })
     })
   }
+  
+  changeSearch = e => {
+    let searchterm = e.target.value.toLowerCase()
+    this.setState({searchterm})
+  }
+
+  sendSearch = e => {
+    if(e.key === "Enter") {
+      let {searchterm} = this.state
+      this.props.history.push(`/busqueda?search=${searchterm}`)
+    }
+  }
 
   render() {
-    let {requests, products} = this.state
+    let {requests, products, searchterm} = this.state
     return (
       <div>
         <h1>Conectamos a gente que quiere rentar algo con gente que lo tiene</h1>
-        <input type="search" name="search" id="searchbar" />
+        <input onChange={this.changeSearch} onKeyDown={this.sendSearch} type="search" name="searchbar" id="searchbar" placeholder="¿Qué quieres rentar?"/>
+        <Link to={`/busqueda?search=${searchterm}`}>
+          <img src="./images/identifica.png" alt="Buscar" height="25"/>        
+        </Link>
         <h2>Ahorra hasta 80% rentando</h2>
         <br />
         <hr />
-        {products.map((product,index)=>{
-          return(
-            <div key={index}>
-              <img src={product.productPics[0]} alt="Producto" height="150"/>
-              <br/>
-              <img src={product.lessor[0].profilePic} alt="Arrendador" height="50" style={{"borderRadius":"50%"}}/>
-              <p>{product.lessor[0].username}</p>
-              <h3>{product.title}</h3>
-              <p>{product.area}</p>
-              <p>desde</p>
-              <h4>${product.rentDay7} por día</h4>
-            </div>
-          )
-        })}
+        <div id="homeproducts" style={{display:"flex"}}>
+          {products.map((product,index)=>{
+            return(
+              <div key={index}>
+                <img src={product.productPics[0]} alt="Producto" height="150"/>
+                <br/>
+                <img src={product.lessor[0].profilePic} alt="Arrendador" height="50" style={{"borderRadius":"50%"}}/>
+                <p>{product.lessor[0].username}</p>
+                <h3>{product.title}</h3>
+                <p>{product.area}</p>
+                <p>desde</p>
+                <h4>${product.rentDay7} por día</h4>
+              </div>
+            )
+          })}
+        </div>
         <h2>Gana dinero extra con las cosas que no usas</h2>
         <br />
         <h2>¿Por qué rentar en Weave?</h2>
         <br />
         <h2>¿No encuentras lo que necesitas?</h2>
         <br />
-        {requests.map((request,index)=>{
-          return(
-            <div key={index}>
-              <img src={request.requester[0].profilePic} alt="ProfilePic" height="50" style={{"borderRadius":"50%"}}/>
-              <h3>{request.requester[0].username}</h3>
-              <p>{request.description}</p>
-            </div>
-          )
-        })}
+        <div id="homerequests" style={{display:"flex"}}>
+          {requests.map((request,index)=>{
+            return(
+              <div key={index}>
+                <img src={request.requester[0].profilePic} alt="ProfilePic" height="50" style={{"borderRadius":"50%"}}/>
+                <h3>{request.requester[0].username}</h3>
+                <p>{request.description}</p>
+              </div>
+            )
+          })}
+        </div>
         
       </div>
     )
