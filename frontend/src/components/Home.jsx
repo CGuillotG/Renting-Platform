838d3f1078e41ee2093be2ddb9f582c73fa46d36
CGuillotG <c.guillot@outlook.com>
2019-03-12 09:15:45 -0600

    Created frontpage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,3 +1,32 @@
 import React from 'react'
+import axios from 'axios'
+let requestsURL = ''
 
-export default () =>  <div>Aqui no van las chelas</div>
+export default class Beers extends React.Component {
+  state = { requests: [] }
+
+  componentDidMount = () => {
+    axios.get(requestsURL).then(res => {
+      this.setState({ requests: res.data })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Conectamos a gente que quiere rentar algo con gente que lo tiene</h1>
+        <input type="search" name="search" id="searchbar" />
+        <h2>Ahorra hasta 80% rentando</h2>
+        <br />
+        <hr />
+        <h2>Gana dinero extra con las cosas que no usas</h2>
+        <br />
+        <h2>¿Por qué rentar en Weave?</h2>
+        <br />
+        <h2>¿No encuentras lo que necesitas?</h2>
+        <br />
+        
+      </div>
+    )
+  }
+}
