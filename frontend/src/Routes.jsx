4c5c956361688885f4ae202bf5f9f842d93ab2a8
CGuillotG <c.guillot@outlook.com>
2019-03-13 11:03:03 -0600

    Added producto and categorias routes

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Switch, Route , Redirect} from "react-router-dom";
-import Home from "./components/Home.jsx";
-import Login from "./components/Login.jsx";
-import Logout from "./components/Logout.jsx";
-import Signup from "./components/Signup.jsx";
-import Profile from "./components/Profile";
-import ProfileEdit from "./components/ProfileEdit.jsx";
-import Construction from "./components/Construction.jsx";
-import Search from "./components/Search.jsx";
+import Home from "./components/Home";
+import Login from "./components/user/Login";
+import Logout from "./components/user/Logout";
+import Signup from "./components/user/Signup";
+import Profile from "./components/account/Profile";
+import ProfileEdit from "./components/account/ProfileEdit";
+import Construction from "./components/Construction";
+import Search from "./components/products/Search";
+import DetailProduct from "./components/products/DetailProduct";
+import Categories from "./components/products/Categories";
+import Category from "./components/products/Category";
 
 export default ({ isLogged, logIn , logOut, user}) => (
   <Switch>
@@ -18,10 +21,13 @@ export default ({ isLogged, logIn , logOut, user}) => (
     <Route exact path="/profile" render={(props => <Profile {...props} user={user} />)} />
     <Route path="/profile/edit" render={(props => <ProfileEdit {...props} user={user} />)} />
     <Route path="/busqueda" component={Search} />
+    <Route path="/producto/:id" component={DetailProduct} />
+    <Route exact path="/categorias" component={Categories} />
+    <Route path="/categorias/:category" component={Category} />
+
 
     <Route path="/construccion" component={Construction} />
-    <Route
-      component={() => {
+    <Route component={() => {
         return <h2>404 - Página no existe</h2>;
       }}
     />
