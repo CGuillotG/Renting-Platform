51a4aa0882a048f172d43f1c16db496125c6970b
CGuillotG <c.guillot@outlook.com>
2019-03-13 11:50:54 -0600

    Added datepicker

--- frontend/src/components/products/DetailProduct.jsx
+++ frontend/src/components/products/DetailProduct.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { DatePicker } from 'antd';
+import moment from 'moment';
+
+
 let productsURL = 'http://localhost:3000/products/'
+const { RangePicker } = DatePicker;
+const dateFormat = 'DD/MM/YYYY';
 
 export default class DetailProduct extends React.Component {
   state = {
-    product: {}
+    product: {},
+    rent: {},
+    error:""
   }
 
   componentDidMount = () => {
@@ -15,6 +23,14 @@ export default class DetailProduct extends React.Component {
       this.setState({ product: res.data[0] })
   })
   }
+  handleChange = (e) =>{
+    let{ rent, error } = this.state
+    rent[e.target.name] = e.target.value
+    error=""
+    if(rent.enddate < rent.startdate) {error="Invalid dates"}
+    this.setState({rent, error})
+    console.log(rent)
+}
 
   render() {
     if(!this.state.product) return <div>Cargando...</div>
@@ -35,57 +51,65 @@ export default class DetailProduct extends React.Component {
     if(availability === "WorkWeek") ww=true
     if(availability === "WeekEnd") we=true
     return (
-      <div>
+      <div >
         <p><Link to="/">weave</Link> > <Link to="/categorias">categorías</Link> > <Link to={`/categorias/${category}`}>{category}</Link> > {title}</p>
-        <img src={mainPic} alt={title} height="300" />
-        {extraPics.map((elem, index)=>{
-          return (
-            <div key={index}>
-              <img src={elem} alt={title} height="50" />
+        <section style={{display:"flex"}}>
+          <div>
+            <img src={mainPic} alt={title} height="300" />
+            {extraPics.map((elem, index)=>{
+              return (
+                <div key={index}>
+                  <img src={elem} alt={title} height="50" />
+                </div>
+              )
+            })}
+          </div>
+          <div> 
+            <h2>{title}</h2>
+            <p>Marca: {brand}</p>
+            <p>{description}</p>
+            <div>
+              <h4>${rentDay1}</h4>
+              <p>por día</p>
             </div>
-          )
-        })}
-        <h2>{title}</h2>
-        <p>Marca: {brand}</p>
-        <p>{description}</p>
-        <div>
-          <h4>${rentDay1}</h4>
-          <p>por día</p>
-        </div>
-        <div>
-          <h4>${rentDay3}</h4>
-          <p>3+ días</p>
-        </div>
-        <div>
-          <h4>${rentDay7}</h4>
-          <p>7+ días</p>
-        </div>
-        
-        <div>
-          <hr/>
-          <h3>¿Cuándo lo necesitas?</h3>
-          <input type="datetime" name="startdate" id="startdate" placeholder="Fecha de Inicio"/>
-          <input type="datetime" name="enddate" id="enddate" placeholder="Fecha Final"/>
-          <table></table>
-          <button>Solicitar Renta</button>
-          <hr/>
-        </div>
-        <div>
-          <h3>Días Disponibles</h3>
-          <ul>
-            {all ? <li>Todos los días</li>: <span></span>}
-            {ww ? <li>Solo entre semana</li>: <span></span>}
-            {we ? <li>Solo fines de semana</li>: <span></span>}
+            <div>
+              <h4>${rentDay3}</h4>
+              <p>3+ días</p>
+            </div>
+            <div>
+              <h4>${rentDay7}</h4>
+              <p>7+ días</p>
+            </div>
+            
+            <div>
+              <hr/>
+              <h3>¿Cuándo lo necesitas?</h3>
+              {/* <input type="datetime" onChange={this.handleChange} name="startdate" id="startdate" placeholder="Fecha de Inicio"/>
+              <input type="datetime" onChange={this.handleChange} name="enddate" id="enddate" placeholder="Fecha Final"/> */}
+              <RangePicker  defaultValue={[moment('15/03/2019', dateFormat), moment('17/03/2019', dateFormat)]} format={dateFormat} />
+              <table></table>
+              <button>Solicitar Renta</button>
+              <hr/>
+            </div>
+            <div>
+              <h3>Días Disponibles</h3>
+              <ul>
+                {all ? <li>Todos los días</li>: <span></span>}
+                {ww ? <li>Solo entre semana</li>: <span></span>}
+                {we ? <li>Solo fines de semana</li>: <span></span>}
 
-          </ul>
-          <h3>Lugar de Entrega</h3>
-          {pickAtAddress ? <ul><li>Vienes a mi casa.</li><li>Área: {area}</li></ul> : <ul><li>Nos ponemos de acuerdo en un punto</li></ul>}
-        </div>
-        <div>
-          <h3>¿Quien lo renta?</h3>
-          <img src={lessor0.profilePic} alt={lessor0.username} height="50"/>
-          <h4>{lessor0.username}</h4>
-        </div>
+              </ul>
+              <h3>Lugar de Entrega</h3>
+              {pickAtAddress ? <ul><li>Vienes a mi casa.</li><li>Área: {area}</li></ul> : <ul><li>Nos ponemos de acuerdo en un punto</li></ul>}
+            </div>
+            <div>
+              <h3>¿Quien lo renta?</h3>
+              <img src={lessor0.profilePic} alt={lessor0.username} height="50"/>
+              <h4>{lessor0.username}</h4>
+            </div>
+          </div>          
+        </section>
+        
       </div>
     )
   }
