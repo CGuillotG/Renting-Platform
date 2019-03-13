0ad0460ca0d8d47ca14c029a7fef45c95c2a739a
CGuillotG <c.guillot@outlook.com>
2019-03-13 11:04:27 -0600

    Added links to products

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -51,11 +51,15 @@ export default class Beers extends React.Component {
           {products.map((product,index)=>{
             return(
               <div key={index}>
-                <img src={product.productPics[0]} alt="Producto" height="150"/>
+                <Link to={`producto/${product._id}`}>
+                  <img src={product.productPics[0]} alt="Producto" height="150"/>
+                </Link>
                 <br/>
                 <img src={product.lessor[0].profilePic} alt="Arrendador" height="50" style={{"borderRadius":"50%"}}/>
                 <p>{product.lessor[0].username}</p>
-                <h3>{product.title}</h3>
+                <Link to={`producto/${product._id}`}>
+                  <h3>{product.title}</h3>
+                </Link>
                 <p>{product.area}</p>
                 <p>desde</p>
                 <h4>${product.rentDay7} por día</h4>
