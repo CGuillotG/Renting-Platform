b3b6c29436fe1377891c5428ecb785f04f1b4609
CGuillotG <c.guillot@outlook.com>
2019-03-13 21:31:47 -0600

    Added format

--- frontend/src/components/account/ProfileLayout.jsx
+++ frontend/src/components/account/ProfileLayout.jsx
@@ -1,33 +1,34 @@
-import React from "react";
-import { Switch, Route, NavLink } from "react-router-dom";
-import Profile from "./Profile";
-import ProfileEdit from "./ProfileEdit";
-import Rents from "./Rents";
-import Publications from "./Publications";
-import DetailRent from "./DetailRent";
+import React from 'react'
+import { Switch, Route, NavLink } from 'react-router-dom'
+import Profile from './Profile'
+import ProfileEdit from './ProfileEdit'
+import Rents from './Rents'
+import Publications from './Publications'
+import DetailRent from './DetailRent'
+import DetailPublication from './DetailPublication'
 
-export default ({user}) => (
-  <div style={{display:"flex"}}>
-    <nav style={{display:"flex", flexDirection:"column"}}>
-        <NavLink activeStyle={{ fontWeight: 'bolder' }} exact to="/cuenta">
-          Mi Perfil
-        </NavLink>
-        <NavLink activeStyle={{ fontWeight: 'bolder' }} to="/cuenta/rentas">
-          Mis Rentas
-        </NavLink>
-        <NavLink activeStyle={{ fontWeight: 'bolder' }} to="/cuenta/publicaciones">
-          Mis Publicaciones
-        </NavLink>
+export default ({ user }) => (
+  <div style={{ display: 'flex' }}>
+    <nav style={{ display: 'flex', flexDirection: 'column' }}>
+      <NavLink activeStyle={{ fontWeight: 'bolder' }} exact to="/cuenta">
+        Mi Perfil
+      </NavLink>
+      <NavLink activeStyle={{ fontWeight: 'bolder' }} to="/cuenta/rentas">
+        Mis Rentas
+      </NavLink>
+      <NavLink activeStyle={{ fontWeight: 'bolder' }} to="/cuenta/publicaciones">
+        Mis Publicaciones
+      </NavLink>
     </nav>
 
     <Switch>
-      <Route exact path="/cuenta" render={(props => <Profile {...props} user={user} />)} />
-      <Route exact path="/cuenta/editar" render={(props => <ProfileEdit {...props} user={user} />)} />
-      <Route exact path="/cuenta/rentas" render={(props => <Rents {...props} user={user} />)} />
+      <Route exact path="/cuenta" render={props => <Profile {...props} user={user} />} />
+      <Route exact path="/cuenta/editar" render={props => <ProfileEdit {...props} user={user} />} />
+      <Route exact path="/cuenta/rentas" render={props => <Rents {...props} user={user} />} />
       <Route path="/cuenta/rentas/:id" component={DetailRent} />
-      <Route exact path="/cuenta/publicaciones" render={(props => <Publications {...props} user={user} />)}/>
-      <Route path="/cuenta/publicaciones/:id" /* component={DetailPublication} */ component={() => <h2>Publicaciones ID</h2>}/>
-      <Route component={() => <h2>404 - Página no existe</h2>}/>
+      <Route exact path="/cuenta/publicaciones" render={props => <Publications {...props} user={user} />} />
+      <Route path="/cuenta/publicaciones/:id" component={DetailPublication} />
+      <Route component={() => <h2>404 - Página no existe</h2>} />
     </Switch>
   </div>
-);
+)
